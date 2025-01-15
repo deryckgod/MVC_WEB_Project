9bd979d4cad373681d513fa30c88b33a5f391c3e
Deryck Ting <deryckgod36@gmail.com>
2025-01-16 01:35:10 +0800

    新增EntityFramework、DB內容加入Models

---
+++ NorthWindProject_MVC/NorthWindProject_MVC/Models/OrderSubtotal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthWindProject_MVC.Models;
+
+public partial class OrderSubtotal
+{
+    public int OrderId { get; set; }
+
+    public decimal? Subtotal { get; set; }
+}
---
+++ NorthWindProject_MVC/NorthWindProject_MVC/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthWindProject_MVC.Models;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}
