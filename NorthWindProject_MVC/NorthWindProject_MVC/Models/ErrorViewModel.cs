5a60f98c9d695fb848a3fdadcd08e0612cd1848e
Deryck Ting <deryckgod36@gmail.com>
2025-01-16 00:45:46 +0800

    新增專案

---
+++ NorthWindProject_MVC/NorthWindProject_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NorthWindProject_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
